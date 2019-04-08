78d062d0a477f0b2d802810eae293deaa87e9b39
ntduoc <tanduoc13>
2019-04-09 01:52:59 +0700

    fix git on git

---
+++ QLThuoc/qlt_reactjs_with_cordovar/qlt_mobile_cordovar/www/precache-manifest.3507811da248bfedd74b2d3c250025df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.2c977d9b.js"
+  },
+  {
+    "revision": "d95527ad32ab7ad4e696",
+    "url": "./static/js/main.8b41664d.chunk.js"
+  },
+  {
+    "revision": "10534ad094e56cc863c6",
+    "url": "./static/js/2.4b44abd2.chunk.js"
+  },
+  {
+    "revision": "d95527ad32ab7ad4e696",
+    "url": "./static/css/main.c47c34ac.chunk.css"
+  },
+  {
+    "revision": "cb71666e56429bb7ed7534ac2fdf0375",
+    "url": "./index.html"
+  }
+];
